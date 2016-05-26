ba9456d5fb845b1cf23ac1fff1eb5b5456fdc657
wdhays2 <wdhays2@charter.net>
2016-05-25 20:56:44 -0700

    wip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,4 @@ class Pin < ActiveRecord::Base
   validates_presence_of :description
   has_attached_file :pin_image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :pin_image, content_type: /\Aimage\/.*\Z/
-  # attr_accessor :pin_image_file_name
 end
