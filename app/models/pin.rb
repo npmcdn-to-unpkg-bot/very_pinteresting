e7586e38be9f663ff7c4b7b3d312f288528dd5e3
wdhays2 <wdhays2@charter.net>
2016-06-02 19:49:27 -0700

    Catch up; big commit.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :description
-  has_attached_file :pin_image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :pin_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :pin_image, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524012610) do
+ActiveRecord::Schema.define(version: 20160602020832) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20160524012610) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "bio"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
