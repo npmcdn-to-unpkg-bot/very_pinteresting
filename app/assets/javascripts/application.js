5fac9503b32e504f4e0e64bf6ebc2c7f31690d56
wdhays2 <wdhays2@charter.net>
2016-06-02 21:06:42 -0700

    try something.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
-//= require masonry/dist/masonry.pkgd
+//= require masonry.pkgd.min
 //= require imagesloaded/imagesloaded.pkgd.min
 //= require turbolinks
 //= require_tree .
