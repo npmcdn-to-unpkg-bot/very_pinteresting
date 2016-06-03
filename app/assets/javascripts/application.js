ada4be1d07fbf8675f3cc80453320d90729fc901
wdhays2 <wdhays2@charter.net>
2016-06-02 20:57:29 -0700

    Added masonry and imagesLoaded.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
+//= require masonry/dist/masonry.pkgd.min
+//= require imagesloaded/imagesloaded.pkgd.min
 //= require turbolinks
 //= require_tree .
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -28,7 +28,7 @@ class PinsController < ApplicationController
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-     redirect_to @pin, notice: 'Pin was created successfully!'
+     redirect_to pins_path, notice: 'Pin was created successfully!'
     else
      render 'new'
     end
