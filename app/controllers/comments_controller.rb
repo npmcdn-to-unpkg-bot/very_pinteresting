f0a8960d56da96de30cf2c967aa34dcda91bee74
wdhays2 <wdhays2@charter.net>
2016-06-22 13:59:19 -0700

    work on errors.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @pin = Pin.find(params[:pin_id])
-    @comment = @pin.comments.create(comment_params)
+    params = comment_params
+    params['user_id'] = current_user.id
+    user = User.find(params[:user_id])
+    @comment = @pin.comments.create(params)
     redirect_to pin_path(@pin)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :boards
+  
   resources :pins do
     resources :comments
     member do
