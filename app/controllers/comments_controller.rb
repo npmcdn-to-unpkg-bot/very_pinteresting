530e8ffb91b91a5189c499d364ea428061b295b9
wdhays2 <wdhays2@charter.net>
2016-06-22 14:23:32 -0700

    Remove unneeded line of code.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,6 @@ class CommentsController < ApplicationController
     @pin = Pin.find(params[:pin_id])
     params = comment_params
     params['user_id'] = current_user.id
-    user = User.find(params[:user_id])
     @comment = @pin.comments.create(params)
     redirect_to pin_path(@pin)
   end
